c58506ad0e12273d06d8ae84487abc0fa233bcab
Marxu <fuhaojun@52tt.com>
2024-06-04 14:29:00 +0800

    feat: 添加 Excel 表头

--- main.py
+++ main.py
@@ -26,6 +26,18 @@ for filepath in filepaths:
     pdf.cell(w=50, h=8, txt=f'Date. {date}', ln=1)
 
     df = pd.read_excel(filepath, sheet_name='Sheet 1')
+
+    # Add the header
+    columns = list(df.columns)
+    pdf.set_font(family='Times', size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=70, h=8, txt=columns[1], border=1)
+    pdf.cell(w=30, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+
+    # Add rows in table
     for index, row in df.iterrows():
         pdf.set_font(family='Times', size=10)
         pdf.set_text_color(80, 80, 80)
